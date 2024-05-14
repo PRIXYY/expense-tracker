9c5efcf856e6d4f0433ed4db40a8dbf4956c49a8
Preet <preet16vaishnav@gmail.com>
2024-05-15 00:55:21 +0530

    Added a form

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from .forms import ExpenseForm
+# Create your views here.
 
-# Create your views here.
-# Create your views here.
 
 def index(request):
-    return render(request,'myapp/index.html')
+    expense_form = ExpenseForm()
+    return render(request,'myapp/index.html',{'expense_form':expense_form})

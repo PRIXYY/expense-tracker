523811c110e1bed8105d98684ee92b66ebfbd7d4
Preet <preet16vaishnav@gmail.com>
2024-05-20 23:45:56 +0530

    Minor Edits

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -6,6 +6,14 @@ Function views
     1. Add an import:  from my_app import views'''
 # Create your models here.
 #Expense model
+
+'''The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views'''
+# Create your models here.
+#Expense model
 class Expense(models.Model):
     name = models.CharField(max_length=100)
     amount = models.IntegerField()
